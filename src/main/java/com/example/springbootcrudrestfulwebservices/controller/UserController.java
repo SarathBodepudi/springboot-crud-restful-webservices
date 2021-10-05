120355a8054493db65fe34efdc0d84f988e0c648
Sarath Bodepudi <sarath.bodepudi@mutualofomaha.com>
2021-10-05 10:07:08 -0500

    remove password

--- src/main/java/com/example/springbootcrudrestfulwebservices/controller/UserController.java
+++ src/main/java/com/example/springbootcrudrestfulwebservices/controller/UserController.java
@@ -1,8 +1,8 @@
 package com.example.springbootcrudrestfulwebservices.controller;
 
-import java.util.ArrayList;
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -15,59 +15,55 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.example.springbootcrudrestfulwebservices.entity.User;
 import com.example.springbootcrudrestfulwebservices.exception.ResourceNotFoundException;
+import com.example.springbootcrudrestfulwebservices.repository.UserRepository;
 
 @RestController
 @RequestMapping("/api/users")
 public class UserController {
-	
+
+	@Autowired
+	private UserRepository userRepository;
+
 	// get all users
 	// http://localhost:8080/api/users
+	// http://springbootcrudrestfulwebservices-env.eba-r2ms64jp.us-east-1.elasticbeanstalk.com/api/users
 	@GetMapping
 	public List<User> getAllUsers() {
-		ArrayList<User> usersList = prepareMockUsers();
-		return usersList;
+		return this.userRepository.findAll();
 	}
 
 	// get user by id
 	// http://localhost:8080/api/users/1
 	@GetMapping("/{id}")
-	public User getUserById(@PathVariable (value = "id") long userId) {
-		if(userId > 10) {
-			throw new ResourceNotFoundException("User Not Found with id :" + userId);
-		}
-		return new User(1000L, "Sarath", "Bodepudi", "sarath.bodepudi@mutualofomaha.com");
+	public User getUserById(@PathVariable(value = "id") long userId) {
+		return this.userRepository.findById(userId)
+				.orElseThrow(() -> new ResourceNotFoundException("User not found with id :" + userId));
 	}
-	
+
 	// save user
 	@PostMapping
 	public User createUser(@RequestBody User user) {
-		return user;
+		return this.userRepository.save(user);
 	}
-	
+
 	// update user
 	@PutMapping("/{id}")
 	public User updateUser(@RequestBody User user, @PathVariable("id") long userId) {
-		User existingUser = new User(user.getId(), user.getFirstName(), user.getLastName(), user.getEmail());
-		return existingUser;
+		User existingUser = this.userRepository.findById(userId)
+				.orElseThrow(() -> new ResourceNotFoundException("User not found with id :" + userId));
+		existingUser.setFirstName(user.getFirstName());
+		existingUser.setLastName(user.getLastName());
+		existingUser.setEmail(user.getEmail());
+		return this.userRepository.save(existingUser);
 	}
-	
+
 	// delete user by id
 	@DeleteMapping("/{id}")
 	public ResponseEntity<User> deleteUser(@PathVariable("id") long userId) {
-		if(userId > 10) {
-			throw new ResourceNotFoundException("User Not Found with id :" + userId);
-		}
+		User existingUser = this.userRepository.findById(userId)
+				.orElseThrow(() -> new ResourceNotFoundException("User not found with id :" + userId));
+		this.userRepository.delete(existingUser);
 		return ResponseEntity.ok().build();
 	}
-	
-	
-	private ArrayList<User> prepareMockUsers() {
-		ArrayList<User> users = new ArrayList<>();
-		
-		User user1 = new User(1000L, "Sarath", "Bodepudi", "sarath.bodepudi@mutualofomaha.com");
-		User user2 = new User(1001L, "Sarath1", "Bodepudi1", "sarathbodepudi@gmail.com");
-		users.add(user1);
-		users.add(user2);
-		return users;
-	}
+
 }
--- src/main/java/com/example/springbootcrudrestfulwebservices/entity/User.java
+++ src/main/java/com/example/springbootcrudrestfulwebservices/entity/User.java
@@ -1,6 +1,6 @@
 package com.example.springbootcrudrestfulwebservices.entity;
 
-/*import javax.persistence.Column;
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
@@ -8,25 +8,28 @@ import javax.persistence.Id;
 import javax.persistence.Table;
 
 @Entity
-@Table(name = "users")*/
+@Table(name = "users")
 public class User {
-	//@Id
-	//@GeneratedValue (strategy = GenerationType.AUTO)
+	@Id
+	@GeneratedValue (strategy = GenerationType.AUTO)
 	private long id;
 	
-	//@Column (name ="first_name")
+	@Column (name ="first_name")
 	private String firstName;
 	
-	//@Column (name ="last_name")
+	@Column (name ="last_name")
 	private String lastName;
 	
-	//@Column (name ="email")
+	@Column (name ="email")
 	private String email;
 	
 	
-	public User(Long id, String firstName, String lastName, String email) {
+	public User() {
+		
+	}
+
+	public User(String firstName, String lastName, String email) {
 		super();
-		this.id = id;
 		this.firstName = firstName;
 		this.lastName = lastName;
 		this.email = email;
